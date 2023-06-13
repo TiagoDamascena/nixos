c984be0dd7ec2bdfac48bd7a2802d5e9142b0423
Tiago Damascena <tiagobdamascena@gmail.com>
2023-06-12 22:46:55 -0300

    VSCode basic settings

--- home/tiago/packages/vscode/default.nix
+++ home/tiago/packages/vscode/default.nix
@@ -10,8 +10,26 @@
     package = pkgs.vscode;
 
     extensions = with vscode-marketplace; [
+      antiantisepticeye.vscode-color-picker
+      gruntfuggly.todo-tree
       maattdd.gitless
       mhutchie.git-graph
+      pkief.material-icon-theme
+      tabnine.tabnine-vscode
+    ];
+
+    userSettings = {
+      "workbench.iconTheme" = "material-icon-theme";
+      "editor.detectIndentation" = true;
+      "editor.tabSize" = 2;
+    };
+
+    keybindings = [
+      {
+        "key" = "ctrl+shift+c";
+        "command" = "-workbench.action.terminal.openNativeConsole";
+        "when" = "!terminalFocus";
+      }
     ];
   };
 }
