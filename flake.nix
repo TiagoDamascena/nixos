4ed36859d914b69724ac441f62d8396bc490835c
Tiago Damascena <tiagobdamascena@gmail.com>
2023-09-18 02:29:43 -0300

    Binary cache added to flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,22 @@
 {
   description = "NixOS de Tiago";
 
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -23,7 +39,7 @@
       url = "github:TiagoDamascena/hyprwm-contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     sddm-sugar-catppuccin = {
       url = "github:TiagoDamascena/sddm-sugar-catppuccin";
       inputs.nixpkgs.follows = "nixpkgs";
